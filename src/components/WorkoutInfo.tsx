ad322848c688c9728dfbcc729d4313713d0c0a93
Markus Vickman <118774957+MarkusVickman@users.noreply.github.com>
2025-09-28 08:57:02 +0200

    Update WorkoutInfo.tsx
    ny hirvis information

--- src/components/WorkoutInfo.tsx
+++ src/components/WorkoutInfo.tsx
@@ -7,8 +7,9 @@ function WorkoutInfo() {
 
         <div className="medium-container">
         <h2>Information</h2>
-        <p>Här finns ditt nya träningsschema! klarmarkera tränade övningar och nollställ veckan när du är klar med alla tre pass. Sedan är det bara att börja om igen.</p>
-        <p>Schemat är endast ett hållschema som inte innehåller specifika övningar. Schemat är tänkt att tränar med pyramid-set och relativt korta pass. Träningsveckan kan vara den längd utövaren önskar och behöver inte vara en vecka lång.</p>
+        <p>Bänkpressprogram är framtaget av en av Sveriges bästa bänkpressare Marcus Hirvonen.</p>
+        <p>Enligt programmet tränar man bänkpress två gånger i veckan i 8 veckor. I programmet utgår man från sitt 1RM, det vill säga den tyngsta vikt du i nuläget klarar av att lyfta en gång. </p>
+        <p>Det går att så klart att träna kompletterande övningar om så önskas. Övningar för övriga kroppen kan med fördel vara grundövningar som knäböj, marklyft och skivstångsrodd som då tränas på andra dagar.</p>
         </div>    
   )
 }
