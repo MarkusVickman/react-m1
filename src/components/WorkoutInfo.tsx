005b36cff7c6a72a9a1e70f244ebd736a0ee60ca
Markus Vickman <118774957+MarkusVickman@users.noreply.github.com>
2025-09-28 09:04:45 +0200

    Update WorkoutInfo.tsx
    text

--- src/components/WorkoutInfo.tsx
+++ src/components/WorkoutInfo.tsx
@@ -9,7 +9,7 @@ function WorkoutInfo() {
         <h2>Information</h2>
         <p>Bänkpressprogram är framtaget av en av Sveriges bästa bänkpressare Marcus Hirvonen.</p>
         <p>Enligt programmet tränar man bänkpress två gånger i veckan i 8 veckor. I programmet utgår man från sitt 1RM, det vill säga den tyngsta vikt du i nuläget klarar av att lyfta en gång. </p>
-        <p>Det går att så klart att träna kompletterande övningar om så önskas. Övningar för övriga kroppen kan med fördel vara grundövningar som knäböj, marklyft och skivstångsrodd som då tränas på andra dagar.</p>
+        <p>Det går att så klart att utöka träningspassen med kompletterande övningar om så önskas. Övningar för övriga kroppen kan med fördel vara grundövningar som knäböj, marklyft och skivstångsrodd som då tränas på andra dagar.</p>
         </div>    
   )
 }
